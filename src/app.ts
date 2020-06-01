764eefca835590d80216ce74cc8cf9c1f87f2b01
Michael Houmann <mhoumann87@gmail.com>
2020-06-01 19:53:38 +0200

    Add ProjectListClass and render function

--- src/app.ts
+++ src/app.ts
@@ -51,6 +51,7 @@ function autobind(
   return adjDescriptor;
 }
 
+// Classes
 
 // Project input class
 class ProjectInput {
@@ -148,4 +149,37 @@ class ProjectInput {
   }
 }
 
+// ProjectList class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+    const importedNode = document.importNode(
+      this.templateElement.content, true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector('ul')!.id = listId;
+    this.element.querySelector('h2')!.textContent =
+      this.type.toUpperCase() + ' PROJECTS';
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element);
+  }
+}
+
 const prjInput = new ProjectInput();
+
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
